4cab4fcff2891ea78d4758dcfe37381f05d5d3a6
Nains <jason20226@gmail.com>
2021-09-03 12:13:11 +0800

    2021-09-03 12:13

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { ResponseData } from '@ts/interface';
 import 'rxjs/add/operator/map';
@@ -17,12 +17,18 @@ export class ApiService {
             .subscribe(Lang => this.langChart = Lang);
     }
     postApi(getWay: string | number, obj?: any): Observable<ResponseData> {
-        let data = (obj) ? this.http.post(`php/system.php?getWay=${getWay}`, this.formateObj(obj)) : this.http.get(`assets/${getWay}.json`);
+        let header = this.formateHeaders({ 'Content-Type': 'application/json; charset=UTF-8' }), option = { headers: header }, // 協定
+            data = (obj) ? this.http.post(`php/system.php?getWay=${getWay}`, this.formateObj(obj), option) : this.http.get(`assets/${getWay}.json`);
         return this.formateData(data);
     }
     private formateData(obj: Observable<any>): Observable<ResponseData> {
         return obj.map((el: HttpResponse<any>) => this.SuccesData(el))
             .catch((err: HttpErrorResponse) => this.ErrorData(err));
+    }
+    private formateHeaders(obj: any): HttpHeaders {
+        let data = new HttpHeaders();
+        Object.keys(obj).forEach(item => data.append(item, obj[item]));
+        return data;
     }
     private formateObj(obj: any): FormData {
         let data = new FormData();
